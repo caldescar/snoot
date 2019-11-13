b0fec53e8be714d04d8c3af4485bc551212acfc5
caldescar <54602987+caldescar@users.noreply.github.com>
2019-11-12 19:08:53 -0500

    its ALMOST PAST TIME

---
+++ e17/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+  console.log('maybe i should get something to eat soon');
+
+
+  $('.fall').click(function() {
+    $('.number1').toggleClass('align-left');
+  });
+
+  $('.drizzle').click(function() {
+    $('.number2').toggleClass('darkcolor');
+  });
+
+  $('.warm').click(function() {
+    $('.warm').toggleClass('hot');
+    $('.number3').toggleClass('hot');
+  });
+
+  $('.lightening').click(function() {
+    $('.number').toggleClass('cool');
+  });
+});
